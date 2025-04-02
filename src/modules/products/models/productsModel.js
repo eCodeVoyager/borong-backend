20a5c9feb42ee00182b1e02889a078640c7cbc26
eCodeVoyager <mu23172@gmail.com>
2025-04-03 05:28:29 +0600

    ADD:: Create product model with schema and pagination support

--- src/modules/products/models/productsModel.js
+++ src/modules/products/models/productsModel.js
@@ -1,1 +1,42 @@
-// productsModel code
+const mongoose = require("mongoose");
+const mongoosePaginate = require("mongoose-paginate-v2");
+
+const productSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+    },
+    image: {
+      type: String,
+    },
+    brand: {
+      type: String,
+    },
+    color: {
+      type: String,
+    },
+    size: {
+      type: String,
+    },
+    description: {
+      type: String,
+    },
+    price: {
+      type: Number,
+    },
+    discount: {
+      type: Number,
+    },
+    stock: {
+      type: Number,
+    },
+    category: {
+      type: String,
+    },
+  },
+  { timestamps: true }
+);
+
+productSchema.plugin(mongoosePaginate);
+
+module.exports = mongoose.model("Product", productSchema);
