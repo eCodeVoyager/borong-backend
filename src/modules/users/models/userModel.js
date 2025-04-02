f17dd232dd0264ed32d4efd100922188c26ff2a3
eCodeVoyager <mu23172@gmail.com>
2025-04-03 05:02:56 +0600

    REFACTOR:: UserModule for current Project

--- src/modules/users/models/userModel.js
+++ src/modules/users/models/userModel.js
@@ -1,60 +1,14 @@
 const bcrypt = require("bcryptjs");
 const mongoose = require("mongoose");
-const mongoosePaginate = require("mongoose-paginate-v2");
 
 const userSchema = new mongoose.Schema(
   {
-    name: { type: String },
     email: { type: String },
-    phone: { type: String },
-    profileImage: { type: String },
-    address: { type: String },
-    // user verfication fields
-    nidNumber: { type: String },
-    nidImage: { type: String },
-    birthCertificateNumber: { type: String },
-    birthCertificateImage: { type: String },
-    birthDate: { type: Date },
-    //system generated fields
     password: { type: String, select: false },
-    isVerified: { type: Boolean, default: false },
-    refreshToken: { type: String, select: false },
-    isActive: { type: Boolean, default: true },
-    role: { type: mongoose.Schema.Types.ObjectId, ref: "Role" },
-    messId: { type: mongoose.Schema.Types.ObjectId, ref: "Mess" },
-    customPermissions: [
-      { type: mongoose.Schema.Types.ObjectId, ref: "Permission" },
-    ],
-
-    // Soft delete fields
-    deletedAt: { type: Date },
-    deleted: { type: Boolean, default: false, select: false },
+    role: { type: String, default: "admin" },
   },
   { timestamps: true }
 );
-
-// Plugins
-userSchema.plugin(mongoosePaginate);
-
-/**
- * Automatically filter out deleted users for find queries
- */
-userSchema.pre(/^find/, function (next) {
-  this.where({ deleted: false });
-  next();
-});
-
-/**
- * Ensure `updateOne`, `findOneAndUpdate`, and similar methods
- * do not modify deleted users and first find the document
- */
-userSchema.pre(["updateOne", "findOneAndUpdate"], async function (next) {
-  const doc = await this.model.findOne(this.getQuery());
-  if (!doc || doc.deleted) {
-    return next(new Error("User not found or has been deleted."));
-  }
-  next();
-});
 
 /**
  * Password hashing before saving the user
@@ -88,17 +42,6 @@ userSchema.methods.changePassword = async function (oldPassword, newPassword) {
   await this.save();
 
   return true;
-};
-
-/**
- * Soft delete a user by ID
- */
-userSchema.statics.softDeleteById = async function (id) {
-  return this.findOneAndUpdate(
-    { _id: id, deleted: false },
-    { deletedAt: new Date(), deleted: true },
-    { new: true }
-  );
 };
 
 module.exports = mongoose.model("User", userSchema);
