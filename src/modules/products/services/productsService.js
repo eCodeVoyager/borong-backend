537f5baf51937112d461d9c726f71026a12df679
eCodeVoyager <mu23172@gmail.com>
2025-04-03 05:31:09 +0600

    ADD:: Implement product service with CRUD operations

--- src/modules/products/services/productsService.js
+++ src/modules/products/services/productsService.js
@@ -1,1 +1,55 @@
-// productsService code
+const productModel = require("../models/productsModel");
+
+const createProduct = async (body) => {
+  try {
+    const product = await productModel.create(body);
+    return product;
+  } catch (error) {
+    throw error;
+  }
+};
+
+const getProducts = async (filter, options) => {
+  try {
+    const products = await productModel.paginate(filter, options);
+    return products;
+  } catch (error) {
+    throw error;
+  }
+};
+
+const getProduct = async (productId) => {
+  try {
+    const product = await productModel.findById(productId);
+    return product;
+  } catch (error) {
+    throw error;
+  }
+};
+
+const updateProduct = async (productId, body) => {
+  try {
+    const product = await productModel.findByIdAndUpdate(productId, body, {
+      new: true,
+    });
+    return product;
+  } catch (error) {
+    throw error;
+  }
+};
+const deleteProduct = async (productId) => {
+  try {
+    const product = await productModel.findByIdAndDelete(productId);
+    return product;
+  } catch (error) {
+    throw error;
+  }
+};
+
+module.exports = {
+  createProduct,
+  getProducts,
+  getProduct,
+  updateProduct,
+  deleteProduct,
+};
